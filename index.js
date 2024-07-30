8cb37dbee708d789e5390ee5c574c792be243862
mizuoya <osgoto.mizu3702@gmail.com>
2024-07-31 01:15:58 +0900

    ボタンを作ったよ

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("こんちわ")
-console.log("おはよう")
+
+const colors = ["red","blue","green","orange","Black"];
+
+function changeColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    document.getElementById("three").style.background = colors[random];
+};
