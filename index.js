8f7114b72f8f001b93a622acfe3d001b21e0276e
mizuoya <osgoto.mizu3702@gmail.com>
2024-08-05 23:49:05 +0900

    最後のボタンを追加しました

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
-const colors = ["red","blue","green","orange","Black"];
+const colors = ["red","blue","green","orange","Black","#569f3c"];
 
 function changeColor() {
     let random = Math.floor(Math.random() * colors.length);
     document.getElementById("three").style.background = colors[random];
+};
+
+function changeColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    document.getElementById("four").style.background = colors[random];
 };
